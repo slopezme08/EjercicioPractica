895c1bff042c72007b6ae308f65f203ef8d33a06
User <User@172.32.34.5>
2023-03-08 13:45:37 -0500

    Primer intento

---
+++ src/main/java/com/mycompany/ejercicios_clase/Ejercicios_Clase.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicios_clase;
+
+public class Ejercicios_Clase {
+
+    public static void main(String[] args) {
+        
+    }
+}
